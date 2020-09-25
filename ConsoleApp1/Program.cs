3a177d45aa8e51d5f8bbfe0a16a76daf00bdf5fe
Usov Alexandr <ussov39@gmail.com>
2020-09-25 21:12:43 +0300

    проверка работы библиотеки

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryLibrary;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Area.TriangleArea(3,4,5));
+            Console.WriteLine(Area.CircleArea(3));
+        }
+
+    }
+}
