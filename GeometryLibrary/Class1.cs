8e5435ed795359cde89c37ed838de5ef41585456
Usov Alexandr <ussov39@gmail.com>
2020-09-25 20:34:39 +0300

    Добавьте файлы проекта.

---
+++ GeometryLibrary/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GeometryLibrary
+{
+    public static class Area
+    {
+        #region StaticMethodsArea
+        public static double TriangleArea(double a, double b, double c)
+        {
+            double p = (a + b + c)/2;
+            return  Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+
+        public static double CircleArea(double r)
+        {
+            return Math.PI * Math.Pow(r,2);
+        }
+
+        #endregion
+
+    }
+}
