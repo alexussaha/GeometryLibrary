4017a0d987ff0ab64528cda99e632efa7ca0a8a1
Usov Alexandr <ussov39@gmail.com>
2020-09-25 21:00:25 +0300

    добавление SQL-запроса

---
+++ SQL_Products_Category/Query.sql
@@ -0,0 +1,3 @@
+﻿select products.name, category.name from Products 
+left join ProductCategory on products.id = productcategory.products_id 
+inner join Category on category.id = productcategory.category_id
